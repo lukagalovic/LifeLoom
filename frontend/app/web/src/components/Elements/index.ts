6fae5c2a6c9efe43782032610a880a026b5e5f81
Luka Galović <Luka.Galovic@holosys.eu>
2024-03-15 09:24:58 +0100

    shared components files

---
+++ frontend/app/web/src/components/Elements/index.ts
@@ -0,0 +1,6 @@
+// export * from "./Button";
+// export * from "./ConfirmationDialog";
+// export * from "./Dialog";
+// export * from "./Link";
+// export * from "./Spinner";
+// export * from "./Table";
